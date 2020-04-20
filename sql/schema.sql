c8cf4a11e95a9b37108a3b073d815e6fffcb3870
Charles Gerard <cngerard@umich.edu>
2020-04-20 07:36:54 -0400

    deleted comments in schema.sql file to avoid errors in aws

--- sql/schema.sql
+++ sql/schema.sql
@@ -40,25 +40,3 @@ CREATE TABLE formanswers(
     FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE,
     FOREIGN KEY(questionid) REFERENCES formquestions(questionid) ON DELETE CASCADE
 );
-
-
-
-#CREATE TABLE formdata(
-#    username      VARCHAR(20)    NOT NULL,
-#    storyid       INTEGER        NOT NULL,
-#    documentid    INTEGER        NOT NULL,
-#    questionid    INTEGER        NOT NULL,
-#    questiontext  VARCHAR(1024)  NOT NULL,
-#    answertext    VARCHAR(1024)          ,
-
-#    PRIMARY KEY(storyid, documentid, questionid),
-#    FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE,
-#    FOREIGN KEY(storyid) REFERENCES stories(storyid) ON DELETE CASCADE,
-#    FOREIGN KEY(documentid) REFERENCES documents(documentid) ON DELETE CASCADE
-#);
-
-# test run below  --> this below should encapsulate same functionality as above code,
-# but with concerns separated and therefore neater and more scalable
-
-#FOR BELOW TABLE
-#PRIMARY KEY(storyid, documentid, questionid), #DO I WANT THIS PRIMARY KEY TO BE ITS OWN UNIQUE ID VARIABLE IN THIS TABLE ??     
