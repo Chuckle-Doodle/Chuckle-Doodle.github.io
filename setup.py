b378487e6d44709c09c1ca45a360241ac131b6c9
Charles Gerard <cngerard@umich.edu>
2020-05-18 19:23:32 -0400

    trying to get setup.py to include everything necessary to run app

--- setup.py
+++ setup.py
@@ -11,18 +11,11 @@ setup(
     packages=['timelineApp'],
     include_package_data=True,
     install_requires=[
-        'arrow==0.13.0',
         'bs4==0.0.1',
-        'Flask==1.0.2',
+        'Flask==1.1.1',
         'Flask-Testing==0.7.1',
-        'html5validator==0.3.1',
-        'nodeenv==1.3.3',
-        'pycodestyle==2.4.0',
-        'pydocstyle==3.0.0',
-        'pylint==2.2.2',
-        'pytest==4.1.1',
+        'nodeenv==1.3.5',
         'requests==2.21.0',
-        'selenium==3.141.0',
-        'sh==1.12.14',
+        'sh==1.12.14'
     ],
 )
